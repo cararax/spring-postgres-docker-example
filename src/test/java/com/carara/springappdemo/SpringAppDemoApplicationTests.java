66ce1be168d086239c97359c0c411417aad858ef
Pedro Carara <cararax@gmail.com>
2022-02-14 07:38:45 -0300

    Projeto criado

---
+++ src/test/java/com/carara/springappdemo/SpringAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carara.springappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
