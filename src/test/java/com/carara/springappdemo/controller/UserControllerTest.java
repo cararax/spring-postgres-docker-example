1fa0f880346fbd1e00b8b359882441c5864be80a
Pedro Carara <cararax@gmail.com>
2022-02-15 19:43:55 -0300

    feat: adicionados testes de exemplo

---
+++ src/test/java/com/carara/springappdemo/controller/UserControllerTest.java
@@ -0,0 +1,29 @@
+package com.carara.springappdemo.controller;
+
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+
+
+    @Test
+    void all() {
+        assertTrue(true);
+    }
+
+    @Test
+    void userById() {
+        assertTrue(true);
+
+    }
+
+    @Test
+    void save() {
+        assertTrue(true);
+
+    }
+}
